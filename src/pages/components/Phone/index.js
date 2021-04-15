90b65ca074055f037ac3a2c5fe99c5893f594e2c
wilmacedo <wil.macedo.sa@gmail.com>
2021-04-15 17:35:53 -0300

    Phone card finished

--- src/pages/components/Phone/index.js
+++ src/pages/components/Phone/index.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
-  Card,
+  Button,
+  CardContainer,
+  CardContent,
   Container,
   Gradient
 } from './styles';
 
 import PhoneImage from '../../../assets/phone.png'
 
-const Phone = () => (
-  <Container>
-    <img src={PhoneImage} alt="Phone" />
-    <Gradient />
-    <Card />
-  </Container>
-);
+const Phone = ({ balance }) => {
+  const [buttonState, setButtonState] = useState(false);
+
+  return (
+    <Container>
+      <img src={PhoneImage} alt="Phone" />
+      <Gradient />
+      <CardContainer>
+        <CardContent>
+          {buttonState ? (
+            <strong>500000</strong>
+          ) : (
+            <p>Waiting for Wallet...</p>
+          )}
+          <span>Balance</span>
+          <Button
+            disabled={!buttonState}
+            onClick={() => setButtonState(!buttonState)}
+          >Auto Update</Button>
+        </CardContent>
+      </CardContainer>
+    </Container>
+  );
+}
 
 export default Phone;
--- src/pages/components/Phone/styles.js
+++ src/pages/components/Phone/styles.js
@@ -1,4 +1,4 @@
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.div`
@@ -13,7 +13,7 @@ export const Gradient = styled.div`
   background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(33,42,48,1) 100%);
 `;
 
-export const Card = styled.div`
+export const CardContainer = styled.div`
   position: absolute;
   top: 55px;
   right: 44px;
@@ -22,4 +22,49 @@ export const Card = styled.div`
   border-radius: 15px;
   height: 210px;
   width: 79%;
+`;
+
+export const CardContent = styled.div`
+  padding: 40px 20px;
+  display: flex;
+  flex-direction: column;
+  
+  span {
+    font-size: 24px;
+    font-weight: 600;
+    color: ${props => props.theme.light};
+  }
+
+  p {
+    font-size: 24px;
+    font-weight: 500;
+    color: ${props => transparentize(.8, props.theme.light)};
+  }
+
+  strong {
+    font-size: 40px;
+    color: ${props => props.theme.light};
+  }
+`;
+
+export const Button = styled.div`
+  position: absolute;
+  bottom: 15px;
+  right: 15px;
+  padding: 5px 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-color: ${props => props.disabled ? 'transparent' : props.theme.primary};
+  border: 1px solid ${props => !props.disabled ? 'transparent' : props.theme.primary};
+  color: ${props => props.theme.light};
+  font-size: 16px;
+  cursor: pointer;
+  transition: .2s ease;
+  border-radius: 5px;
+
+  &:hover {
+    /* box-shadow: 0 1px 6px ${props => transparentize(.1, props.theme.primary)}; */
+  }
 `;
